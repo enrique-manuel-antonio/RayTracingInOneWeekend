9924a09c5e1720c7c74776b7a4e2a0ef56cae352
Enrique <enrique.jqt@gmail.com>
2026-06-09 12:03:52 -0500

    constant color texture & checker texture (solid / spatial)

--- src/RayTracingOneWeekend.cpp
+++ src/RayTracingOneWeekend.cpp
@@ -9,6 +9,7 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
+#include "texture.h"
 
 int main()
 {
@@ -36,8 +37,11 @@ int main()
 
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    //auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    //world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++)
     {
