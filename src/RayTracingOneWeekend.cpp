083f927dbbbfee35b357735df378fdc4fe6a29e1
Enrique <enrique.jqt@gmail.com>
2026-06-11 21:37:15 -0500

    using stb_image & new scene using image texture

--- src/RayTracingOneWeekend.cpp
+++ src/RayTracingOneWeekend.cpp
@@ -127,12 +127,33 @@ void checkered_spheres() {
     cam.render(world);
 }
 
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    camera cam;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0, 0, 12);
+    cam.lookat   = point3(0, 0, 0);
+    cam.vup      = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+    cam.render(hittable_list(globe));
+}
+
 int main() {
     auto start = std::chrono::steady_clock::now();
-    switch (2)
+    switch (3)
     {
-        case 1: bouncing_spheres(); break;
+        case 1: bouncing_spheres();  break;
         case 2: checkered_spheres(); break;
+        case 3: earth();             break;
     }
     auto end = std::chrono::steady_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
